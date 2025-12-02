e66cf178927861f4e2dc7dc0fce3872543ff858f
Cpaz <nicky.shadow99@gmail.com>
2025-12-01 23:02:36 -0500

    initial commit

---
+++ PipeWireMidi/PipeWireMidiManager.cs
@@ -0,0 +1,41 @@
+﻿using Commons.Music.Midi;
+using NLog;
+using System.Net.Sockets;
+using System.Text;
+using XVolume.Factory;
+
+namespace PipeWireMidi;
+
+class PipeWireMidiManager {
+    
+    private static readonly Logger Logger = new LogFactory().GetCurrentClassLogger();
+    
+    static void Main(string[] args) {
+        var volume = VolumeSubsystemFactory.Create();
+
+        // Instantiate the MIDI input device
+        var midiDeviceNumber = 16; // I think this is the client number from "aconnect -l"?
+
+        var access = MidiAccessManager.Default;
+
+        // TODO: Abstract out into UI dropdown list
+        var device = access.Inputs.First(details => details.Id.StartsWith(midiDeviceNumber.ToString()));
+        var input = access.OpenInputAsync(device.Id).Result;
+
+        input.MessageReceived += (sender, eventArgs) => {
+
+            if (sender is not IMidiInput senderInput) {
+                Logger.Error("Received unexpected midi input");
+                return;
+            }
+            // TODO: Somehow parse data being received here...
+            Logger.Debug(eventArgs);
+            
+        };
+
+        Logger.Info("Listening for MIDI input... Press any key to exit.");
+
+        Console.ReadKey();
+        input.CloseAsync();
+    }
+}
